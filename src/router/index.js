d02cebebc298e78c2ae14e29849d7d3e782c0b0f
marinareginato <100595495+marinareginato@users.noreply.github.com>
2023-06-24 17:09:17 -0400

    commit1

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/home/index.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('@/views/sobre/index.vue')
+  }
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('@/views/sobre/index.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
